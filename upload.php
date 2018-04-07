76cd7721ef7187613d79a9cc7339dbdfdfdb913a
yhf7952 <yhf7952@gmail.com>
2018-04-07 14:27:37 +0800

    不显示警告信息

--- upload.php
+++ upload.php
@@ -1,7 +1,7 @@
 <?php
-ini_set('display_errors',1);            //错误信息  
-ini_set('display_startup_errors',1);    //php启动错误信息  
-error_reporting(-1);                    //打印出所有的 错误信息
+//ini_set('display_errors',1);            //错误信息  
+//ini_set('display_startup_errors',1);    //php启动错误信息  
+//error_reporting(-1);                    //打印出所有的 错误信息
 
 // include composer autoload
 require 'vendor/autoload.php';
